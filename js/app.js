c5d2f7d765e3db7fd17d2e0430e051474fb85d16
Kirti Sharma <ks6741948@gmail.com>
2023-08-10 22:11:45 +0530

    commit changes done

--- js/app.js
+++ js/app.js
@@ -23,24 +23,54 @@ hambarger.addEventListener('click',function(){
 times.addEventListener('click',function(){
     mobileNav.classList.remove('open');
 
-}); 
+});
+
 
 
 document.addEventListener('DOMContentLoaded', function() {
-  // Find the "Download CV" button by its class name
-  const downloadBtn = document.querySelector('.download-cv-btn');
-
-  // Replace 'your_cv.pdf' with the actual URL of your CV file
-  const cvURL = 'https://www.example.com/path/to/your_cv.pdf';
-
-  // Attach a click event listener to the button
-  downloadBtn.addEventListener('click', function() {
-    // Create an anchor element
-    const link = document.createElement('a');
-    link.href = cvURL;
-    link.target = '_blank'; // Open the file in a new tab
-    link.download = 'your_cv.pdf'; // Set the suggested name for the downloaded file
-    link.click(); // Simulate a click on the anchor to start the download
+    // Find the "Download CV" button by its class name
+    const downloadBtn = document.querySelector('.download-cv-btn');
+  
+    // Replace 'your_cv.pdf' with the actual URL of your CV file
+    const cvURL = 'cv\resumee4 (1).pdf';
+  
+    // Attach a click event listener to the button
+    downloadBtn.addEventListener('click', function() {
+      // Create an anchor element
+      const link = document.createElement('a');
+      link.href = cvURL;
+      link.target = '_blank'; // Open the file in a new tab
+      link.download = 'resumee4.pdf'; // Set the suggested name for the downloaded file
+      link.click(); // Simulate a click on the anchor to start the download
+    });
   });
-});
+  document.addEventListener("DOMContentLoaded", function () {
+    const sendButton = document.getElementById("send-btn");
+    
+    sendButton.addEventListener("click", function (event) {
+      event.preventDefault(); // Prevent the form from submitting normally
+      
+      const name = document.getElementById("name").value;
+      const email = document.getElementById("email").value;
+      const message = document.getElementById("message").value;
+      
+      // Here you can perform further processing, like sending an HTTP request
+      // to a server to actually send the message.
+      // You can use fetch or other methods to achieve this.
+      
+      // For demonstration purposes, let's just log the data to the console.
+      console.log("Name:", name);
+      console.log("Email:", email);
+      console.log("Message:", message);
+      
+      // You can also show a confirmation message to the user.
+      alert("Message sent successfully!");
+      
+      // Clear the form fields after sending the message.
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+    });
+  });
+  
 });
